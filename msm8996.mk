998970b2da5b084f7abd3da161ed79f2efd00d13
Vol Zhdanov <wight554@gmail.com>
2018-12-30 08:25:52 +0300

    msm8996-common: build libnl for wfd

--- msm8996.mk
+++ msm8996.mk
@@ -421,6 +421,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 # WFD
+PRODUCT_PACKAGES += \
+    libnl
+
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
