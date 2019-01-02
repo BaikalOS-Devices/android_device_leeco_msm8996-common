bce7451a487dbb9389e5acb75e412f4547fe36c4
sdv22 <sdv22@live.ru>
2019-01-02 17:24:28 +0300

    Fix after massive merge

--- msm8996.mk
+++ msm8996.mk
@@ -90,11 +90,15 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio@4.0 \
     android.hardware.audio@4.0-impl \
+    android.hardware.audio@4.0-impl:32 \
     android.hardware.audio.common@4.0 \
     android.hardware.audio.common@4.0-util \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@4.0 \
     android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio.effect@4.0-impl:32 \
+    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.1-impl:32 \
     audio.a2dp.default \
     audio.primary.msm8996 \
     audio.r_submix.default \
