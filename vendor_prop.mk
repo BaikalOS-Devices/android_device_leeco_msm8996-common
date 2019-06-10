1023e012bf9b1e269317df3482590297a14671fb
sdv22 <sdv22@live.ru>
2019-06-10 14:09:15 +0300

    Enable qti perf learning module

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -143,7 +143,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
    ro.vendor.extension_library=libqti-perfd-client.so \
-   ro.vendor.qti.sys.fw.bg_apps_limit=60
+   ro.vendor.qti.sys.fw.bg_apps_limit=60 \
+   debug.vendor.qti.enable.lm=true
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
