aa789e60b717c82b2564bd86a1237c27e57be479
Sergey Vasiliev <sdv22@live.ru>
2020-08-21 20:47:50 +0300

    Add Gaming2 (PUBG) thermal profile. Tune all thermal profiles.

--- msm8996.mk
+++ msm8996.mk
@@ -424,7 +424,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-8996.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8996.conf \
     $(LOCAL_PATH)/configs/thermal-engine-8996-cool.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8996-cool.conf \
-    $(LOCAL_PATH)/configs/thermal-engine-8996-gaming.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8996-gaming.conf
+    $(LOCAL_PATH)/configs/thermal-engine-8996-gaming.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8996-gaming.conf \
+    $(LOCAL_PATH)/configs/thermal-engine-8996-gaming-2.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8996-gaming-2.conf
 
 
 # Trust HAL
